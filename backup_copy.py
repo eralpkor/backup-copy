483d9c833d7f1bee5af82b157fb5949a5732e264
Eralp Kor <eralp.ny@gmail.com>
2016-12-30 14:09:52 -0500

    Add files via upload

---
+++ backup_copy.py
@@ -0,0 +1,33 @@
+# Simple file backup/copy program. Customize your own "BACKUP_FILES & BACKUP_DIRECTORY".
+import datetime
+import os
+import shutil
+
+BACKUP_FILES = '/Users/eralp/test/google' 
+BACKUP_DIRECTORY = '/Users/eralp/test/main/qb_backup_{0}'
+
+def get_backup_directory(base_directory):
+    date = datetime.datetime.now().strftime('%Y-%m-%d-%H%M-%a')
+    return base_directory.format(date)
+
+def copy_files(BACKUP_FILES, BACKUP_DIRECTORY, symlinks=False, ignore=None):
+    for item in os.listdir(BACKUP_FILES):
+        s = os.path.join(BACKUP_FILES, item)
+        d = os.path.join(BACKUP_DIRECTORY, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+def perform_backup(base_directory):
+    backup_directory = get_backup_directory(base_directory)
+    os.makedirs(backup_directory)
+    copy_files(BACKUP_FILES, backup_directory)
+
+def main():
+    perform_backup(BACKUP_DIRECTORY)
+
+if __name__ == '__main__':
+    main()
+
+raw_input("Press enter to exit ;)")
